b958d90d701f5146e28789cbefcb036790d9034d
Siddhant Satkar <ss35627@YSIDEV.YARDI.CLOUD>
2025-03-10 11:30:11 +0530

    Java Practice code

---
+++ Java_Practice/src/encpsulation/FinalKeyword.java
@@ -0,0 +1,16 @@
+package encpsulation;
+
+class Test{
+	
+	final int x = 100;
+}
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test num = new Test();
+		
+		System.out.println(num.x);
+	}
+
+}
---
+++ Java_Practice/src/encpsulation/TestingSuper.java
@@ -0,0 +1,11 @@
+package encpsulation;
+
+public class TestingSuper {
+
+	public static void main(String args []) {
+		
+		Dog check = new Dog();
+		
+		check.eat();
+	}
+}
