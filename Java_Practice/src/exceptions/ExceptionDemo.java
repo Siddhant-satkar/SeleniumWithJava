6bf362346939c42ace251672a73197a4bbff1c97
Siddhant Satkar <ss35627@YSIDEV.YARDI.CLOUD>
2025-03-17 16:38:42 +0530

    exception handling

--- Java_Practice/src/exceptions/ExceptionDemo.java
+++ Java_Practice/src/exceptions/ExceptionDemo.java
@@ -3,27 +3,42 @@ import java.util.*;
 public class ExceptionDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Program is started");
+		/*
+		 * // Example 1
+		 * 
+		 * System.out.println("Program is started");
+		 * 
+		 * Scanner sc = new Scanner(System.in);
+		 * 
+		 * int num = sc.nextInt();
+		 * 
+		 * System.out.println(100/num); // Arithmetic exception
+		 * 
+		 * int a[]=new int[5];
+		 * 
+		 * // Example 2
+		 * 
+		 * System.out.println("Enter the position where you want to add an number"); int
+		 * num1 = sc.nextInt(); System.out.println("Enter the num to add in array"); int
+		 * num2 = sc.nextInt();
+		 * 
+		 * a[num1] = num2; //Array index out of bound exception
+		 * 
+		 * System.out.println("Program is complted");
+		 * System.out.println("Program is exiited");
+		 */
 		
-		Scanner sc = new Scanner(System.in);
+		// Example 3
+		/*
+		String s="Welcome";
+		int num3 = Integer.parseInt(s);  // Number format exception
+		System.out.println(num3);
+		*/
 		
-		int num = sc.nextInt();
+		//Example 4
 		
-		System.out.println(100/num); // Arithmetic exception
-		
-		int a[]=new int[5];
-		
-		System.out.println("Enter the position where you want to add an number");
-		int num1 = sc.nextInt();
-		System.out.println("Enter the num to add in array");
-		int num2 = sc.nextInt();
-		
-		a[num1] = num2;
-		
-		System.out.println("Program is complted");
-		System.out.println("Program is exiited");
+		String name = null;
+		System.out.println(name.length());
 	}
 
 }
