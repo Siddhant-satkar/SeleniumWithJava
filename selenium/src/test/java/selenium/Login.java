d993d73ccf5b135f3a395e353c06d3b1e7d6c740
Siddhant Satkar <ss35627@YSIDEV.YARDI.CLOUD>
2025-03-19 09:35:59 +0530

    Login page automation

---
+++ selenium/src/test/java/selenium/Login.java
@@ -0,0 +1,91 @@
+package selenium;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public String baseUrl = "https://bluepoint.elevateqa.cafe/bluepointnet/content2/dashboard";
+	public WebDriver driver;
+
+	@BeforeTest
+	public void setup() {
+
+		System.out.println("Before Test executed");
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	}
+	@Test
+	public void loginTest() {
+
+		/*
+		 * driver.findElement(By.xpath("//input[@id='Username']")).sendKeys(
+		 * "siddhant.satkar@yardi.com");
+		 * driver.findElement(By.xpath("(//input[@id='txtPassword'])[1]")).sendKeys(
+		 * "HareKrishna@108");
+		 * driver.findElement(By.xpath("//button[@id='loginbtn']")).submit();
+		 */
+		WebElement txtbx_username = driver.findElement(By.id("Username"));
+		
+		txtbx_username.sendKeys("siddhant.satkar@yardi.com");
+		
+		WebElement txtbx_password = driver.findElement(By.id("txtPassword"));
+		
+		txtbx_password.sendKeys("HareKrishna@108");
+		
+		WebElement txtbx_login = driver.findElement(By.id("loginbtn"));
+		
+		txtbx_login.click();
+		
+		String pageTitle = driver.getTitle();
+		if(pageTitle.equalsIgnoreCase("BluePoint")) {
+			System.out.println("Login Sucessfull");
+		}else {
+			System.out.println("Login Failed");
+		}
+	}
+
+	@AfterTest
+	public void tearDown() throws InterruptedException{
+
+		Thread.sleep(5000);
+		driver.close();
+		driver.quit();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * ChromeDriver driver = new ChromeDriver();
+	 * 
+	 * driver.get("https://bluepoint.elevateqa.cafe/bluepointnet/content2/dashboard");
+	 * 
+	 * 
+	 * driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	 * 
+	 * driver.manage().window().maximize(); }
+	 */
+
+}
